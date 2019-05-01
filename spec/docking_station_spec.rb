9cbb9bc1e5341183b6dcae3d083f37774fdd5d73
Student <student@Makerss-MacBook-Pro-6.local>
2019-05-01 15:44:41 +0100

    test bike is working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,18 @@
 require 'docking_station'
 
 describe DockingStation do
+  # it 'does it release' do
+  #   ds = DockingStation.new
+  #   expect(ds.release_bike).to eq(true)
+  # end
 
+  it 'does it respond to release' do
+    expect(DockingStation.new).to respond_to(:release_bike)
+  end
+
+  it 'gives me a bike' do
+    ds = DockingStation.new
+    bike = ds.release_bike
+    expect(bike.working?).to eq(true)
+  end
 end
